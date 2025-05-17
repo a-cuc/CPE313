98abac818d46f5ffd64dd70104f83b5321ddd30c
a-cuc <148326642+a-cuc@users.noreply.github.com>
2025-05-17 15:59:42 +0800

    tset

--- Finals/Project/streamlit_app.py
+++ Finals/Project/streamlit_app.py
@@ -60,7 +60,7 @@ if uploaded_file is not None:
         video_path = tmpfile.name
 
     # Layout with two columns: Video and First Frame
-    col1, col2 = st.columns([1,5])
+    col1, col2 = st.columns([1,1])
 
     with col1:
         st.video(video_path)
