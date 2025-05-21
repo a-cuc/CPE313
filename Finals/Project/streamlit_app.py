30b55d1eedcaebfe91b6af5bb4abb28af0b5b395
a-cuc <148326642+a-cuc@users.noreply.github.com>
2025-05-21 21:41:40 +0800

    Update streamlit_app.py

--- Finals/Project/streamlit_app.py
+++ Finals/Project/streamlit_app.py
@@ -71,7 +71,7 @@ if uploaded_file is not None:
     st.success(f"✅ Extracted {len(frames)} frames.")
 
     with col2:
-        st.image(frames[0], caption="🖼️ First Frame")
+        st.image(frames[0], caption="🖼️ First Frame", use_container_width=True)
 
     # Preprocess frames
     transform = transforms.Compose([
